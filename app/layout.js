8398237fccafcc1fed087bbe1837d72c1f2b5916
Dawood0426 <rd535328@gmail.com>
2025-04-26 00:20:21 -0700

    ye b ak new tag aya ha
    ye analytics ka tag ha

--- app/layout.js
+++ app/layout.js
@@ -9,21 +9,30 @@ export default function RootLayout({ children }) {
       <head>
 
 
-<link
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-  rel="stylesheet"
-/>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
 
-<Script src="your-script.js" defer></Script>
+        <Script src="your-script.js" defer></Script>
 
-<Script
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-  defer
-></Script>
-        
-        
-        
-        </head>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></Script>
+
+
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5ZRTMQ187Y"></Script>
+
+        <Script id="google-analytics">{` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5ZRTMQ187Y');`}</Script>
+
+
+      </head>
       <body>
         <Header></Header>
         {children}
