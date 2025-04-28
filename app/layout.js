927b926590e09284db63fe1fa2c50c60c0aee096
Dawood0426 <rd535328@gmail.com>
2025-04-28 06:24:30 -0700

    ak hor change
    ye adsense kaliya ha

--- app/layout.js
+++ app/layout.js
@@ -6,16 +6,17 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <title>Weather App</title>
-      <link rel="icon" href="/favicon.jpg" />
 
       <head>
 
+
+      <title>Weather App</title>
+      <link rel="icon" href="/favicon.jpg" />
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9296013700822279"
+     crossorigin="anonymous"></script>
+
         <meta name="google-site-verification" content="A4TzlCqngFXjRpTz2ra2t3tHYCueGrH5gL3B6l5guC0" />
-
-
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9296013700822279"
-     crossorigin="anonymous"></Script>
 
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
