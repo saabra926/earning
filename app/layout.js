2b39986726514bc3775be0f6a7068a8db4666168
Dawood0426 <rd535328@gmail.com>
2025-04-28 06:13:45 -0700

    adsense kaliya
    adsense kaliya ha

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,9 @@ export default function RootLayout({ children }) {
 
         <meta name="google-site-verification" content="A4TzlCqngFXjRpTz2ra2t3tHYCueGrH5gL3B6l5guC0" />
 
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9296013700822279"
+     crossorigin="anonymous"></Script>
+
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
